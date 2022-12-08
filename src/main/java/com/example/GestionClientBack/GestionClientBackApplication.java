83a4ecc948c87b07158c3a88cb024bc26b394c8e
admin <saydina18sylla@gmail.com>
2022-12-08 14:03:27 +0000

    Fisrt commit

---
+++ src/main/java/com/example/GestionClientBack/GestionClientBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionClientBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionClientBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionClientBackApplication.class, args);
+	}
+
+}
